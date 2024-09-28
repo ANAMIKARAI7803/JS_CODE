a6fa2a7101ec7b3bf524700e93f682c9f8a89e4e
Anamika Rai <anamikarai7803@gmail.com>
2024-09-29 05:20:15 +0530

    Your message here

---
+++ 3basics/scope.js
@@ -0,0 +1,25 @@
+// var c = 300   //global
+
+// if (true){
+//     let a = 10 
+//     const b = 30 //block scope
+//     var c = 40 
+// }
+
+// console.log(a);
+// console.log(b);
+// console.log(c); //impact 
+
+// blocks 
+// {} problem ha function ka sath ata ha toh scope bolte ha
+
+// var c = 300   //global
+
+let a = 300
+if (true){
+    let a = 10 
+    const b = 30 //block scope
+    console.log("inner:", a);
+    
+}
+console.log(a);
