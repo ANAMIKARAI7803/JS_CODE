d22fdabb80bf15154f0166bc186a1f8cd24a8dc7
Anamika Rai <anamikarai7803@gmail.com>
2024-09-29 05:07:56 +0530

    Your message here

---
+++ 3basics/functions.js
@@ -0,0 +1,50 @@
+// console.log("h");
+// console.log("h");
+// console.log("h");
+// console.log("h");
+// console.log("h");
+// console.log("h");
+
+function sayMyName() {
+    console.log("h");
+    console.log("h");
+    console.log("h");
+    console.log("h");
+    console.log("h");
+    console.log("h");
+}
+
+// sayMyName()
+
+// function addTwoNumber(num1, num2){ //parameters
+
+//     console.log(num1 + num2);
+// }
+
+function addTwoNumber(num1, num2){ //parameters
+
+    // let result = num1 + num2
+    // console.log("anamika");
+    // return result
+
+    // return ka dusra type
+    return num1 + num2
+    
+}
+const result = addTwoNumber(12, "3") //arugments like 12 and 3 
+
+// console.log("results:", result);
+
+
+function loginUsers(username){
+    if(username === undefined){ // hum isko !username likh skte ha same outuput
+    console.log("please enter a user name ");
+    return //agr ek baar return likhdo toh dubra dikkat nhi 
+    
+    }
+    return `${username} just logged`
+}
+// console.log(loginUsers("ana"));  //parameter liy jate ha 
+
+console.log(loginUsers()) ; //  jab koi value pass nhi hoti toh bo undefined output
+
---
+++ 3basics/functions1.js
@@ -0,0 +1,39 @@
+// function calculateCartPrice(...num1){
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+// console.log(calculateCartPrice(200, 400 , 500));
+
+
+// rest or spread use case kab bolenga
+//  rest operator ...calculateCartPrice
+
+
+// convert object in function
+const user = {
+    username : "anamika",
+    age : 23
+}
+function handleObject(anyObject){
+    console.log(`${anyObject.username} ${anyObject.age}`);
+    
+}
+// handleObject(user)
+
+// object banake bhi pass kr pa rhe ha
+
+handleObject({
+    username : "anmma",
+    age : 22
+})
+
+// array ka sath kr skte h
+
+const myNew = [33, 55  ,55 ,544 ,33]
+function returnScValue(getArray){
+    return getArray[1]
+}
+console.log(returnScValue(myNew));
+
+//array , function , object converiosn dekhte ha 
+ 
