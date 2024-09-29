2f7512b19643261376768766a08334b8aa1e1d03
Anamika Rai <anamikarai7803@gmail.com>
2024-09-29 23:24:32 +0530

    Your message here

--- 2basics/Test.js
+++ 2basics/Test.js
@@ -1,13 +1,22 @@
 // positive number practic equstion
 
 
+// let arr = [];
+// const positiven =[1, 3, "anamika" , "5", 7, 8  ];
+// for (let i = 0 ; i < positiven.length; i++){
+//     if(typeof(positiven[i]) === "number"){
+//         if(positiven[i] > 0 ){
+//             arr.push(positiven[i])
+//         }
+//     }
+// }
+// console.log(arr);
+
 let arr = [];
-const positiven =[1, 3, "anamika" , "5", 7, 8  ];
+const positiven =[1, 3, "anamika" , "5", 7, 8, true, '0011' ];
 for (let i = 0 ; i < positiven.length; i++){
-    if(typeof(positiven[i]) === "number"){
-        if(positiven[i] > 0 ){
-            arr.push(positiven[i])
-        }
+    if(typeof(positiven[i]) === "number"){ 
+        arr.push(positiven[i])
     }
 }
 console.log(arr);
