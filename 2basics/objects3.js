dc4f0a71c6e714fdfa570075770377fce5688fc4
Anamika Rai <anamikarai7803@gmail.com>
2024-09-29 23:15:18 +0530

    Your message here

--- 2basics/objects3.js
+++ 2basics/objects3.js
@@ -13,7 +13,7 @@ const course = {
 const {courseInstructor : instructor} =  course
 
 
-// console.log(courseInstructor);
+// console.log(courseInstructor); 
 console.log(instructor);
 
 // Method (React.js)
@@ -23,17 +23,15 @@ const navbar = ({company}) => {
 }
 navbar(company = "ana")
 
-// API = many documentation handle another person 
-
-// JSON -- 
+// API = many documentation handle another person (application programming Interface)
+// forntend and backend connected issey hhota ha API
+// JSON -- (JAvaSCRipt object Notation) == string ko hum JSON form convert krta ha 
 // {} sari value hum string me likhte ha 
 
 
 // convert objects and api in the form of array 
 
 // json format jo sb me use hota ha or orientations based
-
-
 [
     {},
     {},
