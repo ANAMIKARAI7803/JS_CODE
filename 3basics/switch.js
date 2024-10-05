2bd9d61fcf9487647114253eab7d9cc2af86a99f
Anamika Rai <anamikarai7803@gmail.com>
2024-10-06 01:52:09 +0530

    Your message here

---
+++ 3basics/switch.js
@@ -0,0 +1,35 @@
+// switch (key){
+//     case value :
+
+//         break;
+
+//     default :
+//         break
+
+// }
+
+const month = 3 
+//(12 month ka liye check krn eka liye swicth case)
+switch (month){
+    case 1 :
+        console.log("january");
+        break;
+    case  2 :
+        console.log("febraury");
+        break;
+    case  3 :
+        console.log("march");
+        break;
+    case  4 :
+        console.log("april");
+        break;
+
+    default: 
+        console.log("default case execute");
+        
+        break;
+}
+
+//break means march match baki ka match hi nhi kr skte ha jaha case match accept default 
+// ek or baat ki jissey bhi match krwan ha ho toh value or string same likho
+
