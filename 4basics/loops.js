8f83368d14a8393e19a7b6307e6d1e3010561131
Anamika Rai <anamikarai7803@gmail.com>
2024-10-06 02:54:08 +0530

    Your message here

---
+++ 4basics/loops.js
@@ -0,0 +1,25 @@
+// iteration Buat sare tarika hote
+
+// for 
+
+for (let i = 0; i <= 10; i++){
+    const element = i;
+    if (element == 5){
+        //console.log("5 is best number");
+        
+    }
+    //console.log(element);
+}
+
+//,mixed create kr skte ha
+
+//----loop ka andar loop
+for (let i = 0; i < 10 ; i++){
+    // console.log(`outer loop value : ${i}`);
+    for (let j = 0; j < 10 ; j++){
+        // console.log(`Inner value ${j} and inner loop value ${i}`);
+        // console.log(i + ' * ' + j + ' = ' + i * j );
+    }
+}
+
+// ----for loop ---
