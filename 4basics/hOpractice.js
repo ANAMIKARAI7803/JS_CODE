6b5dfaa516fcee0b32922a8308f9f9380dbae738
Anamika Rai <anamikarai7803@gmail.com>
2024-10-15 00:45:41 +0530

    Your message here

---
+++ 4basics/hOpractice.js
@@ -0,0 +1,20 @@
+// const myObject  = {
+//     js : "JavaScript",
+//     cpp : "C++",
+//     rb : "ruby",
+//     Swift : "swift by apple"
+
+// }
+/// ( ye new loop hota ha (for in) )
+// for (const key in myObject) {
+//     console.log(`${key} shortcut is for ${myObject[key]}`);
+    
+// }
+
+// array me kaam kregi 
+
+const program = ["js", "rb", "CPP", "java"]
+//by default agr hum key denga toh ye hume number me output dega
+for(const key in program)
+    {console.log(program[key]);
+}
--- 4basics/highorder.js
+++ 4basics/highorder.js
@@ -21,9 +21,9 @@ for (const greet of greetings) {
 // ----MAPS------(array ki trh or uniqueee value store hota ha)
 // (object annd hold the key value pair)
 // (MOSTLY UNIQUE value ko dekhta ha)
-const map = new Map()
-map.set('IN', 'India')
-map.set('fr', "France")
+// const map = new Map()
+// map.set('IN', 'India')
+// map.set('fr', "France")
 
 // console.log(map);
 // for (const key of map) {  //agr muje aray me value lena ha toh square bracket lga skte ha
@@ -32,8 +32,18 @@ map.set('fr', "France")
 // }
 
 for (const [key, value] of map) {  //agr muje aray me value lena ha toh square bracket lga skte ha
-    console.log(key, '=', value);
+    // console.log(key, '=', value);
     
 }
 
 // object hota tb lga skta kya dekho ab
+
+// (ye error peint krega kyuki ye iterate nhi ha )
+// const map = new Map()
+// map.set('IN', 'India')
+// map.set('fr', "France")
+
+// for (const key in map){
+//     console.log(map);
+    
+// }
