06002b29555ddd13f2e85c214716e900e528fe75
Anamika Rai <anamikarai7803@gmail.com>
2024-10-17 01:23:55 +0530

    Your message here

--- 4basics/foreach.js
+++ 4basics/foreach.js
@@ -1,2 +1,38 @@
 //for each loop kabhi value return hi nhi krta ha
 
+// const coding = ["js", "ruby" , "java", "python", "cpp"]
+
+// const values = coding.forEach( (item) => {
+//     // console.log(item);
+//     return item
+    
+// })
+// console.log(values);
+
+// kai baar hum retun krna chahta ha ya conditional lgti ha
+
+const myNum = [1, ,3,4,5,6,7,8,9]
+
+// filter call back leta ha
+// const newNum = myNum.filter( (num) => num > 5)
+
+//---- const newNum = myNum.filter( (num) => {
+//     return num > 5
+// })
+// --- ye hum curly bracs use kre ha toh object return or scope open return key word use
+// console.log(newNum);
+
+// condition dena pdta ha each access dena hota ha
+
+// for each apko value retuun ni krta jabki ye apko call back krte ha
+
+// const newNums = []
+
+// myNum.forEach( (num) =>  {
+//     if (num > 5) {
+//         newNums.push(num)
+//     }
+// } )
+
+// console.log(newNums);
+
