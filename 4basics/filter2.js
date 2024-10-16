29d08171538f73c43d3249a96bb1543834392e68
Anamika Rai <anamikarai7803@gmail.com>
2024-10-17 01:45:04 +0530

    Your message here

---
+++ 4basics/filter2.js
@@ -0,0 +1,11 @@
+// reduce function
+// accumulator intial value dedeta ha
+
+const mNums = [1,2,3,4]
+
+const myTotal = mNums.reduce(function (acc , currval){
+    console.log(`acc ${acc} and currval: $(currval)`);
+    return acc + currval
+},0)
+
+console.log(myTotal);
